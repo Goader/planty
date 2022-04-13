6a0215f3318246ecf0d234760f346783a139f912
Kamil <61463685+K4m1lP@users.noreply.github.com>
2022-04-14 01:07:30 +0200

    Calendar view update

--- frontend/src/views/CalendarView.tsx
+++ frontend/src/views/CalendarView.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import PlantyNavbar from "../navbar/PlantyNavbar";
+import Footer from "../footer/Footer";
+import SwitchComponent from "../switchComponent/SwitchComponent";
+import './CalendarStyle.css'
 
 function CalendarView() {
+
+    var state = {
+        date: new Date(),
+        locale: "en",
+        style: "calendarStyle"
+    }
+
     return (
-        <div>
-            <Calendar/>
+        <div className={"calendarView"}>
+            <PlantyNavbar/>
+            <SwitchComponent/>
+            <Calendar
+                value={state.date}
+                locale={state.locale}
+                className={state.style}
+            />
+            <Footer/>
         </div>
     )
 }
