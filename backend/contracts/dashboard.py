a3c37d531dc6cf2b37b8326ac0c76f394b69b911
Mykola Haltiuk <kaljandlux@gmail.com>
2022-04-27 21:52:14 +0200

    Plants contracts updated

--- backend/contracts/dashboard.py
+++ backend/contracts/dashboard.py
@@ -11,7 +11,7 @@ class PlantsRequest(BaseModel):
 class Plant(BaseModel):
     id: UUID
     name: str
-    image: bytes  # maybe link to an image?
+    image: str  # maybe link to an image?
     species: str
 
     watering: int
@@ -23,3 +23,48 @@ class Plant(BaseModel):
 # GET /dashboard/plants auth
 class PlantsResponse(BaseModel):
     plants: list[Plant]
+
+
+# POST /dashboard/plants auth
+class PlantsRequest(BaseModel):
+    name: str
+    image: bytes  # base64 encoding
+    species: str
+
+    used_instruction: UUID  # can be blank if the user has entered his own instruction, then a new one will be created
+    watering: int  # can be blank if the used_instruction contains the id
+    insolation: str  # can be blank if the used_instruction contains the id
+    fertilizing: int  # can be blank if the used_instruction contains the id
+
+    other_info: str
+
+# POST /dashboard/plants auth
+class PlantsResponse(BaseModel):
+    pass
+
+
+# PUT /dashboard/plants auth
+class PlantsRequest(BaseModel):
+    id: UUID
+    name: str
+    species: str
+
+    used_instruction: UUID  # can be blank if the user has entered his own instruction, then a new one will be created
+    watering: int  # can be blank if the used_instruction contains the id
+    insolation: str  # can be blank if the used_instruction contains the id
+    fertilizing: int  # can be blank if the used_instruction contains the id
+
+    other_info: str
+
+# POST /dashboard/plants auth
+class PlantsResponse(BaseModel):
+    pass
+
+
+# DELETE /dashboard/plants auth
+class PlantsRequest(BaseModel):
+    id: UUID
+
+# DELETE /dashboard/plants auth
+class PlantsResponse(BaseModel):
+    pass
