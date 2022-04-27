77d9b00b8679772ad54b1d3f383aec101a050ce5
Mykola Haltiuk <kaljandlux@gmail.com>
2022-04-27 18:26:23 +0200

    Mail service [PVD-76] (#1)
    * #PVD-76 added mail service
    
    * docker-compose tutorial
    
    * removing tempfile added
    
    * Mail refactoring

---
+++ backend/mail/app.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import logging
+
+from fastapi import FastAPI
+from fastapi import Body
+
+from models import (
+    SendMailRequest,
+    SendMailResponse
+)
+from mail import Mail
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+app = FastAPI(
+    title='MailServer',
+    version='1.0',
+    description='Mail server acting as a relay for GMail account'
+)
+
+mail = Mail()
+
+
+@app.post('/send', response_model=SendMailResponse)
+async def send(body: SendMailRequest = Body(...)):
+    target = body.target
+    subject = body.subject
+    content = body.content
+
+    logger.info(f'Sending "{subject}" mail to {target}')
+
+    try:
+        mail.send(target, subject, content)
+    except Exception as ex:
+        return {"message": str(ex)}
+
+    logger.info('Successfully sent!')
+
+    return {"message": "done"}
