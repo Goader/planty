956340e58e9b89b9aa2dd566749da7b3474b1820
Mykola Haltiuk <kaljandlux@gmail.com>
2022-04-28 18:52:31 +0200

    calendar events

--- backend/contracts/calendar.py
+++ backend/contracts/calendar.py
@@ -1,19 +1,31 @@
 from uuid import UUID
 
 
-# GET /calendar/events auth
+# GET /dashboard/events auth
 class EventsRequest:
-    start_date: str
-    end_date: str
+    start_date: str  # format YYYY-MM-DD
+    end_date: str  # format YYYY-MM-DD
     
 
 class Event:
     date: str
     plant: UUID
-    action: str
+    action: str  # one out of a few values
     priority: str
+    happened: bool
     message: str
 
-# GET /calendar/events auth
+# GET /dashboard/events auth
 class EventsResponse:
     events: list[Event]
+
+
+# POST /dashboard/events auth
+class EventsRequest:
+    datetime: str  # format YYYY-MM-DD HH:MM:SS
+    plant: UUID
+    action: str  # one out of a few values
+
+# POST /dashboard/events auth
+class EventsResponse:
+    pass
