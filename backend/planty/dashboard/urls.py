af2d75cf7f368fb22885673ef90ce24bbcdb29c4
Piotrek <piotr.kica.35@gmail.com>
2022-05-08 12:47:32 +0200

    add get endpoint for single plant

--- backend/planty/dashboard/urls.py
+++ backend/planty/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PlantsView, EventsView, CustomEventsView
+from .views import PlantsView, EventsView, PlantView, CustomEventsView
 
 urlpatterns = [
+    path('plants/<uuid:id>', PlantView.as_view(), name='PlantView'),
     path('plants/', PlantsView.as_view(), name='PlantsView'),
     path('events/', EventsView.as_view(), name='EventsView'),
     path('events/custom/', CustomEventsView.as_view(), name='CustomEventsView')
