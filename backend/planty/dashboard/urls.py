478fe4c98b69c54263fe5c16dac40decf991892e
pawlew2k <pawlew2k@gmail.com>
2022-05-11 19:26:49 +0200

    changed endpoint name

--- backend/planty/dashboard/urls.py
+++ backend/planty/dashboard/urls.py
@@ -5,5 +5,5 @@ from .views import PlantsView, EventsView, MyInstructionsView
 urlpatterns = [
     path('plants/', PlantsView.as_view(), name='PlantsView'),
     path('events/', EventsView.as_view(), name='EventsView'),
-    path('my_instructions/', MyInstructionsView.as_view(), name='MyInstructionsView')
+    path('dashboard/instructions/', MyInstructionsView.as_view(), name='MyInstructionsView')
 ]
