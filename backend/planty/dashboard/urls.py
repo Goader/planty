0ab68f0338a9d66c30ed18cf6b94e6a7ce933a10
Mykola Haltiuk <kaljandlux@gmail.com>
2022-05-23 19:49:18 +0200

    [PVD-135] added endpoint for custom events (#22)

--- backend/planty/dashboard/urls.py
+++ backend/planty/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PlantsView, EventsView
+from .views import PlantsView, EventsView, CustomEventsView
 
 urlpatterns = [
     path('plants/', PlantsView.as_view(), name='PlantsView'),
-    path('events/', EventsView.as_view(), name='EventsView')
+    path('events/', EventsView.as_view(), name='EventsView'),
+    path('events/custom/', CustomEventsView.as_view(), name='CustomEventsView')
 ]
